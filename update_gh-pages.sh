f036ba9ece8aa205ea8f56488bdbc504144308b1
Yoshimura Yuu <yyu@mental.poker>
2023-03-24 14:55:55 +0900

    Fix

--- update_gh-pages.sh
+++ update_gh-pages.sh
@@ -4,6 +4,14 @@ set -eo
 
 TARGET="scalamatsuri2023"
 
+if [[ ! $(git rev-parse --verify gh-pages 2>/dev/null) ]]; then
+  git checkout --orphan gh-pages
+  git rm -rf .
+  git clean -fxd
+  git commit --allow-empty -m 'Dummy commit'
+  git checkout master
+fi
+
 make
 
 if [ $? != 0 ]; then
